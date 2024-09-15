af6966ffbf1a170308ffab3cedeeecfbf8b6ec72
Osanyem Osadebe <osanyemo@gmail.com>
2024-09-15 15:13:24 -0400

    generated new spring project

---
+++ src/main/java/com/market/citymarket/api/CitymarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.citymarket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitymarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitymarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/citymarket/api/CitymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.citymarket.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitymarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
